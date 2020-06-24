b9f52ee401047639434362583003d7b3c7ce5d91
aschong <amdachong@gmail.com>
2020-06-24 16:52:13 -0700

    Adding all files at once

---
+++ analysis.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+load("rda/murders.rda")
+
+murders %>% mutate(abb = reorder(abb,rate)) %>%
+  ggplot(aes(abb,rate)) +
+  geom_bar(width = 0.5, stat = "identity", color="black")+
+  coord_flip()
+
+#open git bash and mkdir figs directory to save barplot
+ggsave("figs/barplot.png")
